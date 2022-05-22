5f8ee20ddf2f4fe27f7eb82abe70c80c595737ba
William Harrington <wrharringtonjr@gmail.com>
2022-05-21 18:17:51 -0700

    Update example_poll.py

--- examples/example_poll.py
+++ examples/example_poll.py
@@ -3,8 +3,6 @@
 Example use of ADXL345 module
 
 In this example the X,Y,Z values are polled on the ADXL345
-Numpy is then used to convert them in to signed 16-bit integers
-Samples are taken approximately every 300ms
 
 Programmed by William Harrington
 
