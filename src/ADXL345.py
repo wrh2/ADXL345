5b6d6452406495748e58d860e7bf388b2937b4f2
William Harrington <wrharringtonjr@gmail.com>
2022-05-22 19:18:43 -0700

    Refactor
    - Register map now internal to module
    - Cleaned up all the global constants, appended ADXL345_ to avoid collisions
    - Specify bitrate for spi transactions

--- src/ADXL345.py
+++ src/ADXL345.py
@@ -9,7 +9,6 @@ Programmed by William Harrington
 wrh2.github.io
 
 """
-
 from ctypes import c_int16
 import spidev
 import RPi.GPIO as GPIO
@@ -28,86 +27,58 @@ SPI_DEVICE = DEFAULT_SPI_DEVICE
 CS_PIN = DEFAULT_CS_PIN
 CS_PIN_MODE = DEFAULT_CS_PIN_MODE
 
-READ = 1
-WRITE = 0
+"""
+Note from datasheet
 
-# RW bit position
-RW_SHIFT = 7
+Use of the 3200 Hz and 1600 Hz output data rates is only recommended with SPI communication
+rates greater than or equal to 2 MHz.
 
-# multibyte bit position
-MB_SHIFT = 6
+The 800 Hz output data rate is recommended only for communication speeds greater than or equal to 400 kHz,
+and the remaining data rates scale proportionally.
 
-# register addresses
-WHO_AM_I = 0x0
-BW_RATE = 0x2C
-POWER_CTL = 0x2D
-DATA_FORMAT = 0x31
-DATAX0 = 0x32
-DATAY0 = 0x34
-DATAZ0 = 0x36
-
-CPOL_HIGH = 1
-CPOL_LOW = 0
-
-CPHA_LEAD = 0
-CPHA_TRAIL = 1
-
-CPOL = CPOL_HIGH
-CPHA = CPHA_TRAIL
-CPOL_SHIFT = 1
+For example, the minimum recommended communication speed for a 200 Hz output data rate is 100 kHz.
+"""
+ADXL345_DEFAULT_SPI_BITRATE = int(2e6) # 2MHz
 
 ADXL345_WHO_AM_I_VAL = 0xE5
 
-MEASURE_MODE = 1
-MEASURE_SHIFT = 3
+ADXL345_ACC_SCALE_2G = 0
+ADXL345_ACC_SCALE_4G = 1
+ADXL345_ACC_SCALE_8G = 2
+ADXL345_ACC_SCALE_16G = 3
 
-FULL_RES = 1
-FULL_RES_SHIFT = 3
+ADXL345_ACC_SENSITIVITY_2G = 3.9 * 1e-3 * 9.81 # mg/LSB
+ADXL345_ACC_SENSITIVITY_4G = 7.8 * 1e-3 * 9.81
+ADXL345_ACC_SENSITIVITY_8G = 15.6 * 1e-3 * 9.81
+ADXL345_ACC_SENSITIVITY_16G = 31.2 * 1e-3 * 9.81
 
-SELF_TEST = 1
-SELF_TEST_SHIFT = 7
+ADXL345_OUTPUT_DATA_RATE_0_10HZ = 0x0
+ADXL345_OUTPUT_DATA_RATE_0_20HZ = 0x1
+ADXL345_OUTPUT_DATA_RATE_0_39HZ = 0x2
+ADXL345_OUTPUT_DATA_RATE_0_78HZ = 0x3
+ADXL345_OUTPUT_DATA_RATE_1_56HZ = 0x4
+ADXL345_OUTPUT_DATA_RATE_3_13HZ = 0x5
+ADXL345_OUTPUT_DATA_RATE_6_25HZ = 0x6
+ADXL345_OUTPUT_DATA_RATE_12_5HZ = 0x7
+ADXL345_OUTPUT_DATA_RATE_25HZ = 0x8
+ADXL345_OUTPUT_DATA_RATE_50HZ = 0x9
+ADXL345_OUTPUT_DATA_RATE_100HZ = 0xA
+ADXL345_OUTPUT_DATA_RATE_200HZ = 0xB
+ADXL345_OUTPUT_DATA_RATE_400HZ = 0xC
+ADXL345_OUTPUT_DATA_RATE_800HZ = 0xD
+ADXL345_OUTPUT_DATA_RATE_1600HZ = 0xE
+ADXL345_OUTPUT_DATA_RATE_3200HZ = 0xF
 
-G = 9.81
-
-ACC_SCALE_2G = 0
-ACC_SCALE_4G = 1
-ACC_SCALE_8G = 2
-ACC_SCALE_16G = 3
-
-ACC_SENSITIVITY_2G = 3.9 * 1e-3 * G # mg/LSB
-ACC_SENSITIVITY_4G = 7.8 * 1e-3 * G
-ACC_SENSITIVITY_8G = 15.6 * 1e-3 * G
-ACC_SENSITIVITY_16G = 31.2 * 1e-3 * G
-
-OUTPUT_DATA_RATE_100HZ = 0xA
-OUTPUT_DATA_RATE_200HZ = 0xB
-OUTPUT_DATA_RATE_400HZ = 0xC
-OUTPUT_DATA_RATE_800HZ = 0xD
-OUTPUT_DATA_RATE_1600HZ = 0xE
-OUTPUT_DATA_RATE_3200HZ = 0xF
-
-DEFAULT_SENSITIVITY = ACC_SENSITIVITY_2G
-DEFAULT_SCALE = ACC_SCALE_2G
-DEFAULT_ODR = OUTPUT_DATA_RATE_100HZ
-
-X_DATA_SIZE = 2    # bytes
-Y_DATA_SIZE = 2
-Z_DATA_SIZE = 2
-XYZ_DATA_SIZE = 6
-BW_RATE_DATA_SIZE = 1
-WHO_AM_I_DATA_SIZE = 1
-DATA_FORMAT_SIZE = 1
+ADXL345_DEFAULT_SENSITIVITY = ADXL345_ACC_SENSITIVITY_2G
+ADXL345_DEFAULT_SCALE = ADXL345_ACC_SCALE_2G
+ADXL345_DEFAULT_ODR = ADXL345_OUTPUT_DATA_RATE_100HZ
 
 def MAKE_INT16(x,y):
     return ((x << 8) | y)
 
-# def TWOS_COMPLEMENT(x, bits=16):
-#     mask = 2**(bits-1)
-#     return -(x & mask) + (x & ~mask)
-
 class ADXL345:
 
-    def __init__(self, sensitivity=DEFAULT_SENSITIVITY, scale=DEFAULT_SCALE, odr=DEFAULT_ODR, full_resolution=True, software_cs=False):
+    def __init__(self, sensitivity=ADXL345_DEFAULT_SENSITIVITY, scale=ADXL345_DEFAULT_SCALE, odr=ADXL345_DEFAULT_ODR, full_resolution=True, software_cs=False, bitrate=ADXL345_DEFAULT_SPI_BITRATE):
         
        # sets up private constants
        self.__constants(sensitivity, scale, full_resolution, software_cs)
@@ -122,9 +93,46 @@ class ADXL345:
        self.__whoAmI = None
        self.__getWhoAmI()
        self.__configure_accelerometer(odr, scale)
+       self.__bitrate = bitrate
+
+    def __setupRegisterMap(self):
+
+        # register map for ADXL345
+        self.regs = {
+            'DEVID': 0x0,
+            'THRESH_TAP': 0x1D,
+            'OFSX': 0x1E,
+            'OFSY': 0x1F,
+            'OFSZ': 0x20,
+            'DUR': 0x21,
+            'Latent': 0x22,
+            'Window': 0x23,
+            'THRESH_ACT': 0x24,
+            'THRESH_INACT': 0x25,
+            'TIME_INACT': 0x26,
+            'ACT_INACT_CTL': 0x27,
+            'THRESH_FF': 0x28,
+            'TIME_FF': 0x29,
+            'TAP_AXES': 0x2A,
+            'ACT_TAP_STATUS': 0x2B,
+            'BW_RATE': 0x2C,
+            'POWER_CTL': 0x2D,
+            'INT_ENABLE': 0x2E,
+            'INT_MAP': 0x2F,
+            'INT_SOURCE': 0x30,
+            'DATA_FORMAT': 0x31,
+            'DATAX0': 0x32,
+            'DATAX1': 0x33,
+            'DATAY0': 0x34,
+            'DATAY1': 0x35,
+            'DATAZ0': 0x36,
+            'DATAZ1': 0x37,
+            'FIFO_CTL': 0x38,
+            'FIFO_STATUS': 0x39,
+        }
+
 
     def __exit__(self):
-        
         if self.__software_cs:
             GPIO.cleanup()
 
@@ -137,7 +145,7 @@ class ADXL345:
             # according to ADXL345 datasheet
             # in full resolution mode the sensitivity
             # is always 3.9mg/LSB
-            self.__sensitivity = ACC_SENSITIVITY_2G
+            self.__sensitivity = ADXL345_ACC_SENSITIVITY_2G
 
         else:
 
@@ -153,6 +161,21 @@ class ADXL345:
         GPIO.output(CS_PIN, GPIO.HIGH)
        
     def __spi_setup(self):
+
+        # clock polarity
+        CPOL_LOW = 0
+        CPOL_HIGH = 1
+
+        # clock phase
+        CPHA_LEAD = 0
+        CPHA_TRAIL = 1
+
+        # CPHA = 1, CPOL =1
+        CPOL = CPOL_HIGH
+        CPHA = CPHA_TRAIL
+
+        CPOL_SHIFT = 1
+
         self.__spi = spidev.SpiDev()
         self.__spi.open(SPI_BUS, SPI_DEVICE)
         self.__spi.mode = (CPOL << CPOL_SHIFT) | CPHA
@@ -171,12 +194,18 @@ class ADXL345:
         self.__measurement_setup()
 
     def __odr_setup(self, odr):
+        BW_RATE = self.regs['BW_RATE']
         self.__write_data(BW_RATE, [odr])
 
     def __full_res(self, scale):
+        FULL_RES_SHIFT = 3
+        DATA_FORMAT = self.regs['DATA_FORMAT']
         self.__write_data(DATA_FORMAT, [(self.__full_resolution << FULL_RES_SHIFT) | scale])
 
     def __measurement_setup(self):
+        MEASURE_MODE = 1
+        MEASURE_SHIFT = 3
+        POWER_CTL = self.regs['POWER_CTL']
         self.__write_data(POWER_CTL, [(MEASURE_MODE << MEASURE_SHIFT)])
 
     def __multibyte(self, d):
@@ -187,10 +216,13 @@ class ADXL345:
         """
         This function is used for reading bytes from the ADXL345
         """
+        ADXL345_READ_BIT = 1
+        ADXL345_RW_SHIFT = 7 # RW bit position
+        ADXL345_MB_SHIFT = 6 # multibyte bit position
 
         MB = self.__multibyte(L)
         
-        msg = (READ << RW_SHIFT) | (MB << MB_SHIFT) | addr
+        msg = (ADXL345_READ_BIT << ADXL345_RW_SHIFT) | (MB << ADXL345_MB_SHIFT) | addr
 
         dummy_bytes = [0xFF for i in range(L)]
 
@@ -200,7 +232,7 @@ class ADXL345:
         if self.__software_cs:
             GPIO.output(CS_PIN, GPIO.LOW)
 
-        result = self.__spi.xfer(to_send)
+        result = self.__spi.xfer(to_send, self.__bitrate)
 
         # for software controlled chip select
         if self.__software_cs:
@@ -212,10 +244,14 @@ class ADXL345:
         """
         This function is used for writing bytes from the ADXL345
         """
+        ADXL345_WRITE_BIT = 0
+        ADXL345_RW_SHIFT = 7 # RW bit position
+        ADXL345_MB_SHIFT = 6 # multibyte bit position
+
 
         MB = self.__multibyte(len(d))
         
-        msg = (WRITE << RW_SHIFT) | (MB << MB_SHIFT) | addr
+        msg = (ADXL345_WRITE_BIT << ADXL345_RW_SHIFT) | (MB << ADXL345_MB_SHIFT) | addr
 
         to_write = [msg] + d
 
@@ -223,7 +259,7 @@ class ADXL345:
         if self.__software_cs:
             GPIO.output(CS_PIN, GPIO.LOW)
             
-        self.__spi.xfer(to_write)
+        self.__spi.xfer(to_write, self.__bitrate)
 
         # for software controlled chip select
         if self.__software_cs:
@@ -248,6 +284,8 @@ class ADXL345:
         This function reads the WHO_AM_I register
         and returns the data to the user
         """
+        WHO_AM_I = self.regs['DEVID']
+        WHO_AM_I_DATA_SIZE = 1
         data = self.__read_data(WHO_AM_I, WHO_AM_I_DATA_SIZE)
 
         self.__whoAmI = data[0]
@@ -269,6 +307,9 @@ class ADXL345:
         
     def getX(self, raw=True):
         
+        X_DATA_SIZE = 2
+        DATAX0 = self.regs['DATAX0']
+
         data = self.__read_data(DATAX0, X_DATA_SIZE)
         
         result = MAKE_INT16(data[1], data[0])
@@ -279,6 +320,9 @@ class ADXL345:
         return result
 
     def getY(self, raw=True):
+
+        Y_DATA_SIZE = 2
+        DATAY0 = self.regs['DATAY0']
         
         data = self.__read_data(DATAY0, Y_DATA_SIZE)
         
@@ -290,6 +334,9 @@ class ADXL345:
         return result
 
     def getZ(self, raw=True):
+
+        Z_DATA_SIZE = 2
+        DATAZ0 = self.regs['DATAZ0']
         
         data = self.__read_data(DATAZ0, Z_DATA_SIZE)
         
@@ -301,6 +348,9 @@ class ADXL345:
         return result
 
     def getXYZ(self, raw=True):
+
+        XYZ_DATA_SIZE = 6
+        DATAX0 = self.regs['DATAX0']
 
         data = self.__read_data(DATAX0, XYZ_DATA_SIZE)
 
